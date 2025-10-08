f96345d50979f844f5721ec94fa9c53de424966f
alexandrektm <alexandrektmitsuuchi@gmail.com>
2025-10-08 17:34:06 -0300

    Create 2025_10_08.py

---
+++ 2025_10/2025_10_08.py
@@ -0,0 +1,25 @@
+# Space Week Day 5: Goldilocks Zone
+# For the fifth day of Space Week, you will calculate the "Goldilocks zone" of a star - the region around a star where conditions are "just right" for liquid water to exist.
+#
+# Given the mass of a star, return an array with the start and end distances of its Goldilocks Zone in Astronomical Units.
+#
+# To calculate the Goldilocks Zone:
+#
+# Find the luminosity of the star by raising its mass to the power of 3.5.
+# The start of the zone is 0.95 times the square root of its luminosity.
+# The end of the zone is 1.37 times the square root of its luminosity.
+# Return the distances rounded to two decimal places.
+# For example, given 1 as a mass, return [0.95, 1.37].
+
+
+
+def goldilocks_zone(mass):
+
+    luminosity = mass**3.5
+    start_zone = (luminosity**0.5)*0.95
+    end_zone = (luminosity**0.5)*1.37
+    result = [float(f"{start_zone:.2f}"),float(f"{end_zone:.2f}")]
+
+    return result
+
+print(goldilocks_zone(1))
