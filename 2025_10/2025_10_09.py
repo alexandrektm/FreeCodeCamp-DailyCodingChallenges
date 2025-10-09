abe6f5f673785a50a7708cc641f369c1195c99a9
alexandrektm <alexandrektmitsuuchi@gmail.com>
2025-10-09 14:15:18 -0300

    Create 2025_10_09.py

---
+++ 2025_10/2025_10_09.py
@@ -0,0 +1,8 @@
+def moon_phase(date_string):
+
+    temporary_list = date_string.strip("").split("-")
+    print(temporary_list)
+
+    if (int(temporary_list[0]) % 4 == 0) and not :
+    
+print(moon_phase("2006-01-06"))
