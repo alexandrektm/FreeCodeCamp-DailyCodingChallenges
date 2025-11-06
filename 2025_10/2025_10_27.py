570e3c6c636576271f72a3269c468ac70def6d6a
alexandrektm <alexandrektmitsuuchi@gmail.com>
2025-11-05 22:13:42 -0300

    Create 2025_10_27.py

---
+++ 2025_10/2025_10_27.py
@@ -0,0 +1,16 @@
+# Integer Sequence
+# Given a positive integer, return a string with all of the integers from 1 up to, and including, the given number, in numerical order.
+# 
+# For example, given 5, return "12345".
+
+def sequence(n):
+
+        listing = list(range(n+1))[1::]
+        new_list = []
+        for element in listing:
+                new_list.append(f"{element}")
+        final_list = "".join(new_list)
+
+        return final_list
+
+print(sequence(27))
